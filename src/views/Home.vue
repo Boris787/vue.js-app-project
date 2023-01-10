c86e8b6675e54e185f8f769e12cdad0f5a9e3040
Boris787 <borismrdja78@gmail.com>
2023-01-10 13:14:31 +0100

    creating home page, added axios, tailwindcss forms, API

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,26 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8">
+    <div class="items-center justify-center">
+      <input type="text" class="rounded border-2 order-gray-200 w-full" placeholder="Search for Meals">
+    </div>
+    <div class="flex justify-center gap-2 mt-2">
+      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
+import { computed, onMounted } from "vue";
 import store from "../store"
+import axiosClient from '../axiosClient.js'
 
 
-const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split("");
 
+onMounted(async () => {
+  const response = await axiosClient.get('/list.php?i=list')
+  console.log(response.data)
+})
 </script>
